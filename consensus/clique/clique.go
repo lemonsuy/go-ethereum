24e284e73ad76e54c989b4fab7696a5aaf0529d1
pattharathon srithundorn <lemon.pattharathon@gmail.com>
2022-01-11 16:38:37 +0700

    Remove unused  verify logic

--- consensus/clique/clique.go
+++ consensus/clique/clique.go
@@ -347,17 +347,6 @@ func (c *Clique) verifyCascadingFields(chain consensus.ChainHeaderReader, header
 	if err != nil {
 		return err
 	}
-	// If the block is a checkpoint block, verify the signer list
-	if number%c.config.Epoch == 0 {
-		signers := make([]byte, len(snap.Signers)*common.AddressLength)
-		for i, signer := range snap.signers() {
-			copy(signers[i*common.AddressLength:], signer[:])
-		}
-		extraSuffix := len(header.Extra) - extraSeal
-		if !bytes.Equal(header.Extra[extraVanity:extraSuffix], signers) {
-			return errMismatchingCheckpointSigners
-		}
-	}
 	// All basic checks passed, verify the seal and return
 	return c.verifySeal(snap, header, parents)
 }
